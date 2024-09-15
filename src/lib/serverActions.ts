466db633fc2ccb3c4f85a619d98ac7d5457d911b
Otim Isaac <isaacomega16@gmail.com>
2024-09-16 01:59:31 +0300

    fetch logs from database

--- src/lib/serverActions.ts
+++ src/lib/serverActions.ts
@@ -3,6 +3,7 @@
 import { ILog } from '@/Types';
 import {supabase} from './superbaseClient'
 
+
 export async function storeLog(visitor : ILog){
     const response = await supabase
       .from('logs')
@@ -28,19 +29,47 @@ export async function fetchBusinesses(){
   return data
 }
 export async function fetchLogs(){
-    const { data, error } = await supabase
-  .from('logs')
-  .select()
+  const { data, error } = await supabase
+  .from("logs")
+  .select(`
+    id,
+    name,
+    finger_print_url AS fingerPrint,
+    id_number AS idNumber,     
+    reason,
+    time_in AS timeIn,     
+    time_out AS timeOut,    
+    
+    business:business_id (
+      id ,
+      name ,
+      location 
+    )
+  `);
   if(error)
     return 'error fetching businesses'
-  return data
+  return data as unknown as ILog[]
 }
 export async function fetchBusinessLogs(businessId : number){
     const { data, error } = await supabase
   .from('logs')
-  .select()
+  .select(`
+    id,
+    name,
+    finger_print_url AS fingerPrint,
+    id_number AS idNumber,     
+    reason,
+    time_in AS timeIn,     
+    time_out AS timeOut  
+    
+   
+  `)
   .eq('business_id', businessId)
   if(error)
     return 'error fetching businesses'
-  return data
+  return data as unknown as ILog[]
 }
+
+
+
+
