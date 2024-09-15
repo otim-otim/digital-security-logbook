96c875d4093e264c1d017bb3faa186e38c97b74d
Otim Isaac <isaacomega16@gmail.com>
2024-09-16 01:57:26 +0300

    add shad components

--- src/lib/superbaseClient.ts
+++ src/lib/superbaseClient.ts
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://dnzerdgnilttyxfyzzyi.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY as string // Make sure the key is stored in environment variables
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuemVyZGduaWx0dHl4Znl6enlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY0MDg1NDksImV4cCI6MjA0MTk4NDU0OX0.wOxIi2A7O0F-hMu8oDlpollwf2RQ0kgETBJLlGIgtxo'
 export const supabase = createClient(supabaseUrl, supabaseKey)
