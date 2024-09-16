d8d4b5aef9368fb9171a69674329be44fd655d93
Otim Isaac <isaacomega16@gmail.com>
2024-09-16 03:42:27 +0300

    add tabs for navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
 import Image from "next/image";
 import VisitorLogin from "@/components/VisitorLogin";
 import VisitorLogs from "@/components/VisitorLogs";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+      <Tabs defaultValue="account" className="w-full">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="logs">Logs</TabsTrigger>
+        <TabsTrigger value="newLog">Log new Visitor</TabsTrigger>
+      </TabsList>
+      <TabsContent value="logs">
+        <VisitorLogs  />
+      </TabsContent>
+      <TabsContent value="newLog">
+      <VisitorLogin  />
+      </TabsContent>
+    </Tabs>
+
+        
         
 
-        <VisitorLogin  />
-        <VisitorLogs  />
+       
+       
       </main>
       {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
